e61f147a21f0eda14cce6fc29cc8d885fda7e978
Geoffrey Ostosh <gostosh@brightsign.biz>
2018-03-18 12:21:33 -0700

    add decl bugfix, cleanup

--- bin/shared-run.js
+++ bin/shared-run.js
@@ -1,29 +1,30 @@
 #!/usr/bin/env node
 
-const findDeps = require('../lib/findDeps'),
-    runlib = require('../lib/runlib'),
-    style = require('../lib/style'),
-    argv = require('minimist')(process.argv.slice(2), {
-        boolean: ['parallel', 'no-prod', 'dev'],
-        string: ['filter'],
-        default: {
-            p: false,
-            np: false,
-            d: false
-        },
-        alias: {
-            parallel: 'p',
-            'no-prod': 'np',
-            dev: 'd',
-            filter: 'f'
-        }
-    });
+const findDeps = require('../lib/findDeps');
+const runlib = require('../lib/runlib');
+const style = require('../lib/style');
+const argv = require('minimist')(process.argv.slice(2), {
+    boolean: ['parallel', 'no-prod', 'dev'],
+    string: ['filter'],
+    default: {
+        p: false,
+        np: false,
+        d: false
+    },
+    alias: {
+        parallel: 'p',
+        'no-prod': 'np',
+        dev: 'd',
+        filter: 'f'
+    }
+});
 
-const runS = runlib.runS,
-    runP = runlib.runP;
+const runS = runlib.runS;
+const runP = runlib.runP;
 
+let command = '';
 if (argv._.length > 0) {
-    const command = argv._.join(' ');
+    command = argv._.join(' ');
 }
 
 const deps = findDeps(process.cwd(), argv.np, argv.d, argv.f);
@@ -37,13 +38,11 @@ if (deps.length !== 0) {
 
         if (argv.p) {
             runP(deps, command).catch((err) => { throw err; });
-        }
-        else {
+        } else {
             runS(deps, command);
         }
     }
 
-}
-else {
+} else {
     console.log(style.warn("No local dependencies found."));
 }
